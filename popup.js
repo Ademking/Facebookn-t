3495e09d7da38802cb7c4de4d7b9d8ac6560c18a
JoeNguyen93 <joe.nguyen@softagram.com>
2018-06-10 01:46:02 +0700

    Add time limit feature

--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-
 /*
 
 
@@ -33,7 +32,7 @@ function myFunction(url) {
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    
+
 
     chrome.tabs.getSelected(null, function(tab) {
         myFunction(tab.url);
@@ -62,9 +61,18 @@ document.addEventListener('DOMContentLoaded', function() {
     var status;
 
     // 2. Get Status Stored in Storage API
-    chrome.storage.sync.get(['status'], function(items) {
+    chrome.storage.sync.get(['status', 'turnOnTime', 'limitTime'], function(items) {
 
         console.log('Etat Status : ', items['status']);
+
+        // Check timing
+        var limitTimeInSec = items['limitTime'] * 60;
+        var timePassedInSec = (Date.now() - items['turnOnTime']) / 1000;
+        if (timePassedInSec < limitTimeInSec) {
+            $('#btn-toggle').css('display', 'none');
+        } else {
+            $('#btn-toggle').css('display', 'block');
+        }
 
         status = items['status'];
 
@@ -72,9 +80,14 @@ document.addEventListener('DOMContentLoaded', function() {
         if (status == false ) {
             $('#btn-toggle').addClass('btn-secondary');
             $('#btn-toggle').html(`<i class="fas fa-play"></i>&nbsp;&nbsp;Turn On`);
+
+            // Show the time form
+            $('.time-form-container').css('display', 'inline');
+
             chrome.browserAction.setBadgeText({
                 'text': '' //an empty string displays nothing!
             });
+
             $('#status_text').text("Disabled");
        }
     
@@ -82,6 +95,9 @@ document.addEventListener('DOMContentLoaded', function() {
             $('#btn-toggle').addClass('btn-tertiary');
             $('#btn-toggle').html(`<i class="fas fa-stop"></i>&nbsp;&nbsp;Turn OFF`);
             $('#status_text').text("Enabled");
+
+            // Hide time form
+            $('.time-form-container').css('display', 'none');
 
             chrome.browserAction.setBadgeBackgroundColor({color: "red"})
             chrome.browserAction.setBadgeText({text: "ON"}); 
@@ -102,16 +118,29 @@ document.addEventListener('DOMContentLoaded', function() {
     $("#btn-toggle").click(function() {
         //when status is False -> turn on 
         if (status == false) {
+            // Hide the button
+            $('#btn-toggle').css('display', 'none');
+            
+            // Change button style (to TURN OFF style)
             $('#btn-toggle').addClass('btn-tertiary').removeClass('btn-secondary');
-           
             $('#btn-toggle').html(`<i class="fas fa-stop"></i>&nbsp;&nbsp;Turn OFF`);
+
+            // Get limit time and hide the form
+            var limitTime = $('#time-form').val();
+            $('.time-form-container').css('display', 'none');
+
+            // Change status
             $('#status_text').text("Enabled");
             status = true;
+
             chrome.browserAction.setBadgeBackgroundColor({color: "red"})
             chrome.browserAction.setBadgeText({text: "ON"});            
+
             // Save it To extension storage API.
-            chrome.storage.sync.set({'status': true}, function() {
+            chrome.storage.sync.set({'status': true, 'turnOnTime': Date.now(), 'limitTime': limitTime}, function() {
                 console.log('Status = True - Settings saved');
+                console.log('Turn On Time saved');
+                console.log('Limit Time saved');
             });
         
         }
@@ -119,8 +148,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
             //when status is True -> turn OFF 
 
+            // Change button style (to TURN ON style)
             $('#btn-toggle').addClass('btn-secondary').removeClass('btn-tertiary');
             $('#btn-toggle').html(`<i class="fas fa-play"></i>&nbsp;&nbsp;Turn On`);
+
+            // Show the time form
+            $('.time-form-container').css('display', 'inline');
+
+            // Change status
             $('#status_text').text("Disabled");
             status = false;
             chrome.browserAction.setBadgeText({
